e3cd7a8f40bfe49a31976f61eb5893e64c8b7f6f
Dylan Renwick <skidsirongenesisdev@gmail.com>
2018-07-30 10:55:46 +0100

    Changed command extensions to follow style guide

--- cmdExts/droll.js
+++ cmdExts/droll.js
@@ -13,10 +13,10 @@ module.exports = function (bot, message) {
 	if (!command) return;
 
 	if (Object.keys(commands).includes(command.command)) {
-		log.info("Received command " + commands[command.command].name + " with args: " + command.args.join(', '));
+		log.info('Received command ' + commands[command.command].name + ' with args: ' + command.args.join(', '));
 		commands[command.command](bot, message, command.args);
 	}
-}
+};
 
 function processCommand(message) {
 	let content = message.content;
@@ -33,15 +33,15 @@ function roll(bot, message, args) {
 	let diceString = args.join(' ');
 	let result = dice.roll(diceString);
 	if (diceString.indexOf('d') > 4) {
-		message.channel.send("I can't handle that many dice, please choose a number less than 10,000");
+		message.channel.send('I can\'t handle that many dice, please choose a number less than 10,000');
 		return;
 	}
 	if (result) {
-		let msg = "Result is: [" + result.rolls.join(' + ') + "] = " + result.total;
-		if (msg.length >= 2000) msg = "Total is: " + result.total + ", there were too many dice to show";
+		let msg = 'Result is: [' + result.rolls.join(' + ') + '] = ' + result.total;
+		if (msg.length >= 2000) msg = 'Total is: ' + result.total + ', there were too many dice to show';
 		message.channel.send(msg);
 	} else {
-		message.channel.send("Sorry, I'm not sure what you mean");
-		log.error("Invalid dice string: " + diceString);
+		message.channel.send('Sorry, I\'m not sure what you mean');
+		log.error('Invalid dice string: ' + diceString);
 	}
 }
--- cmdExts/roles.js
+++ cmdExts/roles.js
@@ -13,10 +13,10 @@ module.exports = function (bot, message) {
 	if (!command) return;
 
 	if (Object.keys(commands).includes(command.command)) {
-		log.info("Received command " + command.command + " with args: " + command.args.join(', '));
+		log.info('Received command ' + command.command + ' with args: ' + command.args.join(', '));
 		commands[command.command](bot, message, command.args);
 	}
-}
+};
 
 function processCommand(message) {
 	let content = message.content;
@@ -33,15 +33,15 @@ function addrole(bot, message, args) {
 	let roleName = args[0];
 	if (roleName.startsWith('#')) {
 		if (!/^#(?:[0-9a-fA-F]{3}){1,2}$/.test(roleName)) {
-			message.channel.send("Sorry, that is not a valid color");
+			message.channel.send('Sorry, that is not a valid color');
 			return;
 		}
 		if (roleName.length == 4) roleName = '#' + roleName.substring(1).repeat(2);
-		log.info("Role is color: " + roleName);
+		log.info('Role is color: ' + roleName);
 		createRoleIfNotExist(roleName.toLowerCase(), message);
 	} else {
-		log.info("Invalid role?");
-		message.channel.send("You can currently only add colors, sorry!");
+		log.info('Invalid role?');
+		message.channel.send('You can currently only add colors, sorry!');
 	}
 }
 
@@ -50,37 +50,35 @@ function createRoleIfNotExist(roleName, message) {
 	let member = message.member;
 	let role = guild.roles.findAll('name', roleName);
 	if (role.length != 0) {
-		log.info("Role exists");
+		log.info('Role exists');
 		guild.member(member).addRole(role[0]).then(member => {
-			log.info("Role added!");
-			message.channel.send("Role added :+1:");
+			log.info('Role added!');
+			message.channel.send('Role added :+1:');
 		});
 		return;
 	}
 
-	log.info("Role doesn't exist");
+	log.info('Role doesn\'t exist');
 	let botRole = guild.roles.findAll('name', 'Bot');
 	if (botRole.length === 0) {
-		log.error("Could not find bot role!");
+		log.error('Could not find bot role!');
 	}
 
-	//Array.from(guild.roles.values()).sort((a, b) => b.position - a.position).forEach(r => log.info(r.name + ' '.repeat(32 - r.name.length) + ', ' + r.calculatedPosition));
-
 	let botPos = botRole[0].calculatedPosition;
-	log.info("Bot pos is " + botPos);
-	
-	log.info("Creating role");
+	log.info('Bot pos is ' + botPos);
+
+	log.info('Creating role');
 	guild.createRole({
 		name: roleName,
 		color: roleName
 	}).then(role => {
-		log.info("Role created");
+		log.info('Role created');
 		guild.setRolePosition(role, botPos).then(guild => {
-			log.info("Role moved");
+			log.info('Role moved');
 			let role = Array.from(guild.roles.values()).filter(role => role.name.toLowerCase() === roleName)[0];
 			member.addRole(role).then(member => {
-				log.info("Role added!");
-				message.channel.send("Role added :+1:");
+				log.info('Role added!');
+				message.channel.send('Role added :+1:');
 			});
 		});
 	});
@@ -88,24 +86,24 @@ function createRoleIfNotExist(roleName, message) {
 
 function removerole(bot, message, args) {
 	let roleName = args[0];
-	log.info("Removing role " + roleName);
+	log.info('Removing role ' + roleName);
 	let member = message.member;
 	let role = message.guild.roles.findAll('name', roleName);
 	if (role.length == 0) {
-		log.info("Role not found");
-		message.channel.send("Role " + roleName + " does not exist!");
+		log.info('Role not found');
+		message.channel.send('Role ' + roleName + ' does not exist!');
 		return;
 	}
 
 	if (!member.roles.has(role[0].id)) {
-		log.info("User does not have role");
-		message.channel.send("You don't have that role!");
+		log.info('User does not have role');
+		message.channel.send('You don\'t have that role!');
 		return;
 	}
 
-	log.info("User has role");
+	log.info('User has role');
 	member.removeRole(role[0]).then(member => {
-		log.info("Role removed");
-		message.channel.send("Role removed :+1:");
+		log.info('Role removed');
+		message.channel.send('Role removed :+1:');
 	});
 }
