1daaea94af8afde41896f68ea8258b5b0fb43cd4
Dylan Renwick <skidsirongenesisdev@gmail.com>
2018-07-31 11:32:56 +0100

    Added role whitelisting, reformatted code

--- cmdExts/roles.js
+++ cmdExts/roles.js
@@ -5,13 +5,56 @@ const storage = require('../util/storage.js');
 const guildData = storage.currentGuilds;
 
 const commands = {
-	roleadd: addrole,
-	rolegive: addrole,
-	roleremove: removerole,
-	roledel: removerole,
-	rolerm: removerole,
-	roleaddstaff: addstaffrole
+	add: {
+		action: roles.add,
+		aliases: [
+			'add',
+			'give',
+			'grant',
+			'a'
+		]
+	},
+	remove: {
+		action: roles.del,
+		aliases: [
+			'remove',
+			'del',
+			'rm',
+			'delete',
+			'r'
+		]
+	},
+	addstaff: {
+		action: staff.add,
+		aliases: [
+			'addstaff'
+		]
+	},
+	removestaff: {
+		action: staff.del,
+		aliases: [
+			'removestaff'
+		]
+	},
+	whitelist: {
+		action: whitelist.add,
+		aliases: [
+			'whitelist',
+			'wl'
+		]
+	},
+	unwhitelist: {
+		action: whitelist.del,
+		aliases: [
+			'unwhitelist',
+			'uwl',
+			'blacklist',
+			'bl'
+		]
+	}
 };
+
+const typePrefix = 'role';
 
 module.exports = function (bot, message) {
 	let command = processCommand(message);
@@ -19,123 +62,221 @@ module.exports = function (bot, message) {
 
 	if (Object.keys(commands).includes(command.command)) {
 		log.info('Received command ' + command.command + ' with args: ' + command.args.join(', '));
-		commands[command.command](bot, message, command.args);
+		command.action(bot, message, command.args);
 	}
 };
 
 function processCommand(message) {
 	let content = message.content;
-	if (!content.startsWith(config.bot.prefix + 'role')) return null;
+	if (!content.startsWith(config.bot.prefix + typePrefix)) return null;
 	let contentArr = content.split(' ');
-	let command = contentArr.shift().substring(1);
+	let command = contentArr.shift().substring(config.bot.prefix);
+	if (command !== typePrefix) command = command.substring(typePrefix.length);
+
+	let foundCommands = commands.filter(c => c.aliases.includes(command));
+	if (foundCommands.length === 0) return null;
+	if (foundCommands.length > 1) log.warning('Multiple commands found with alias ' + command + ', selecting first');
+	let runCommand = foundCommands[0];
+
 	return {
+		action: runCommand.action,
 		command: command,
 		args: contentArr
 	};
 }
 
-function addrole(bot, message, args) {
-	let roleName = args[0];
-	if (roleName.startsWith('#')) {
-		if (!isColorRole(roleName)) {
-			message.channel.send('Sorry, that is not a valid color');
+const roles = {
+	add: function (bot, message, args) {
+		let roleName = args[0];
+		if (roleName.startsWith('#')) {
+			if (!roles.isColorRole(roleName)) {
+				message.channel.send('Sorry, that is not a valid color');
+				return;
+			}
+			if (roleName.length == 4) roleName = '#' + roleName.substring(1).repeat(2);
+			log.info('Role is color: ' + roleName);
+			roles.addColorRole(roleName.toLowerCase(), message);
+		} else {
+			roles.addRegularRole(roleName, message);
+		}
+	},
+	addColorRole: function (roleName, message) {
+		let guild = message.guild;
+		let member = message.member;
+		let role = guild.roles.find('name', roleName);
+		if (role) {
+			guild.member(member).addRole(role[0]).then(member => {
+				message.channel.send('Color changed :+1:');
+			});
 			return;
 		}
-		if (roleName.length == 4) roleName = '#' + roleName.substring(1).repeat(2);
-		log.info('Role is color: ' + roleName);
-		createRoleIfNotExist(roleName.toLowerCase(), message);
-	} else {
-		log.info('Invalid role?');
-		message.channel.send('You can currently only add colors, sorry!');
-	}
-}
 
-function createRoleIfNotExist(roleName, message) {
-	let guild = message.guild;
-	let member = message.member;
-	let role = guild.roles.findAll('name', roleName);
-	if (role.length != 0) {
-		log.info('Role exists');
-		guild.member(member).addRole(role[0]).then(member => {
-			log.info('Role added!');
-			message.channel.send('Role added :+1:');
-		});
-		return;
-	}
+		let botRole = guild.roles.find('name', 'Bot');
+		if (!botRole) {
+			return log.error('Could not find bot role!');
+		}
 
-	log.info('Role doesn\'t exist');
-	let botRole = guild.roles.findAll('name', 'Bot');
-	if (botRole.length === 0) {
-		log.error('Could not find bot role!');
-	}
+		let botPos = botRole[0].calculatedPosition;
 
-	let botPos = botRole[0].calculatedPosition;
-	log.info('Bot pos is ' + botPos);
-
-	log.info('Creating role');
-	guild.createRole({
-		name: roleName,
-		color: roleName
-	}).then(role => {
-		log.info('Role created');
-		guild.setRolePosition(role, botPos).then(guild => {
-			log.info('Role moved');
-			let role = Array.from(guild.roles.values()).filter(role => role.name.toLowerCase() === roleName)[0];
-			member.addRole(role).then(member => {
-				log.info('Role added!');
-				message.channel.send('Role added :+1:');
+		guild.createRole({
+			name: roleName,
+			color: roleName
+		}).then(role => {
+			log.info('Created role ' + roleName);
+			guild.setRolePosition(role, botPos).then(guild => {
+				let role = Array.from(guild.roles.values()).filter(role => role.name.toLowerCase() === roleName)[0];
+				member.addRole(role).then(member => {
+					let memberRoles = Array.from(member.roles);
+					for(let i = 0; i < memberRoles.length; i++) {
+						let role = roles[i];
+						if (roles.isColorRole(role.name)) member.removeRole(role);
+					}
+					message.channel.send('Color changed :+1:');
+				});
 			});
 		});
-	});
-}
+	},
+	addRegularRole: function (roleName, message) {
+		if (!guildData.has(message.guild.id)) storage.buildGuild(message.guild);
+		let guildSettings = guildData.get(message.guild.id);
+		let roleData = (guildSettings.roles) ? guildSettings.roles : {};
+		let allowedRoles = (roleData.whitelist) ? roleData.whitelist : [];
 
-function removerole(bot, message, args) {
-	let roleName = args[0];
-	log.info('Removing role ' + roleName);
-	let member = message.member;
-	let role = message.guild.roles.findAll('name', roleName);
-	if (role.length == 0) {
-		log.info('Role not found');
-		message.channel.send('Role ' + roleName + ' does not exist!');
-		return;
+		if (!allowedRoles.includes(roleName)) return message.channel.send('Sorry, you can\'t add that role');
+
+		let role = message.guild.roles.find('name', roleName);
+		if (!role) return message.channel.send('I\'ve been told I can give you this role, but I can\'t find it, let the admins know :frowning:');
+
+		let member = message.member;
+		member.addRole(role).then(member => {
+			message.channel.send('Role added :+1:');
+		});
+	},
+	del: function (bot, message, args) {
+		let roleName = args[0];
+		log.info('Removing role ' + roleName);
+		let member = message.member;
+		let role = message.guild.roles.findAll('name', roleName);
+		if (role.length == 0) {
+			log.info('Role not found');
+			message.channel.send('Role ' + roleName + ' does not exist!');
+			return;
+		}
+
+		if (!member.roles.has(role[0].id)) {
+			log.info('User does not have role');
+			message.channel.send('You don\'t have that role!');
+			return;
+		}
+
+		let roleIsEmpty = (role[0].members.length === 1);
+
+		log.info('User has role');
+		member.removeRole(role[0]).then(member => {
+			log.info('Role removed');
+			message.channel.send('Role removed :+1:');
+
+			if (roleIsEmpty && roles.isColorRole(role[0].name)) role[0].delete('No users left in color role');
+		});
+	},
+	isColorRole: function (roleName) {
+		return (/^#(?:[0-9a-fA-F]{3}){1,2}$/.test(roleName));
 	}
+};
 
-	if (!member.roles.has(role[0].id)) {
-		log.info('User does not have role');
-		message.channel.send('You don\'t have that role!');
-		return;
+const staff = {
+	add: function (bot, message, args) {
+		if (message.member.id !== message.guild.ownerID) return message.channel.send('You don\'t have permission to do that!');
+		let roleName = args.join(' ');
+		let role = message.guild.roles.find('name', roleName);
+		if (!role) return message.channel.send('Sorry, I can\'t find the role "' + roleName + '"');
+
+		if (!guildData.has(message.guild.id)) storage.buildGuild(message.guild);
+		let guildSettings = guildData.get(message.guild.id);
+		let roleData = (guildSettings.roles) ? guildSettings.roles : {};
+		let staffRoles = (roleData.staff) ? roleData.staff : [];
+
+		staffRoles.push(role);
+		roleData.staff = staffRoles;
+		guildSettings.roles = roleData;
+		guildData.set(message.guild.id, guildSettings);
+
+		return message.channel.send(roleName + ' was whitelisted as a staff role :+1:');
+	},
+	del: function (bot, message, args) {
+		if (message.member.id !== message.guild.ownerID) return message.channel.send('You don\'t have permission to do that!');
+		let roleName = args.join(' ');
+		let role = message.guild.roles.find('name', roleName);
+		if (!role) return message.channel.send('Sorry, I can\'t find the role "' + roleName + '"');
+
+		if (!guildData.has(message.guild.id)) storage.buildGuild(message.guild);
+		let guildSettings = guildData.get(message.guild.id);
+		let roleData = (guildSettings.roles) ? guildSettings.roles : {};
+		let staffRoles = (roleData.staff) ? roleData.staff : [];
+
+		if (staffRoles.indexOf(roleName) < 0) return message.channel.send('That role isn\'t a staff role');
+
+		staffRoles = staffRoles.slice(0, staffRoles.indexOf(roleName)).concat(staffRoles.slice(staffRoles.indexOf(roleName) + 1));
+		roleData.staff = staffRoles;
+		guildSettings.roles = roleData;
+		guildData.set(message.guild.ind, guildSettings);
+
+		return message.channel.send(roleName + ' was removed from the staff whitelist :+1:');
+	},
+	getIsStaff: function (member, guild) {
+		// TODO: Check for staff roles on member
+		if (member.id === guild.ownerID) return true;
+		if (!guildData.has(guild.id)) storage.buildGuild(guild);
+		let guildSettings = guildData.get(guild.id);
+		let roleData = (guildSettings.roles) ? guildSettings.roles : {};
+		let staffRoles = (roleData.staff) ? roleData.staff : [];
+
+		let memberRoles = member.roles;
+		for(let i = 0; i < staffRoles.length; i++) {
+			if (memberRoles.find('name', staffRoles[i])) return true;
+		}
+
+		return false;
 	}
+};
 
-	let roleIsEmpty = (role[0].members.length === 1);
+const whitelist = {
+	add: function (bot, message, args) {
+		if (!staff.getIsStaff(message.member, message.guild)) return message.channel.send('You don\'t have permission to do that!');
+		let roleName = args.join(' ');
+		let role = message.guild.roles.find('name', roleName);
+		if (!role) return message.channel.send('Sorry, I can\'t find the role "' + roleName + '"');
 
-	log.info('User has role');
-	member.removeRole(role[0]).then(member => {
-		log.info('Role removed');
-		message.channel.send('Role removed :+1:');
+		if (!guildData.has(message.guild.id)) storage.buildGuild(message.guild);
+		let guildSettings = guildData.get(message.guild.id);
+		let roleData = (guildSettings.roles) ? guildSettings.roles : {};
+		let allowedRoles = (roleData.whitelist) ? roleData.whitelist : [];
 
-		if (roleIsEmpty && isColorRole(role[0].name)) role[0].delete('No users left in color role');
-	});
-}
+		allowedRoles.push(role);
+		roleData.whitelist = allowedRoles;
+		guildSettings.roles = roleData;
+		guildData.set(message.guild.id, guildSettings);
 
-function addstaffrole(bot, message, args) {
-	if (message.member.id !== message.guild.ownerID) return message.channel.send('You don\'t have permission to do that!');
-	let roleName = args.join(' ');
-	let role = message.guild.roles.find('name', roleName);
-	if (!role) return message.channel.send('Sorry, I can\'t find the role "' + roleName + '"');
-	if (!guildData.has(message.guild.id)) storage.buildGuild(message.guild);
-	let guildSettings = guildData.get(message.guild.id);
-	let roleData = (guildSettings.roles) ? guildSettings.roles : {};
-	let staffRoles = (roleData.staff) ? roleData.staff : [];
-	staffRoles.push(role);
-	roleData.staff = staffRoles;
-	guildSettings.roles = roleData;
-	guildData.set(message.guild.id, guildSettings);
-}
+		return message.channel.send(roleName + ' was whitelisted, members can now add this role with ' + config.bot.prefix + 'roleadd :+1:');
+	},
+	del: function (bot, message, args) {
+		if (!staff.getIsStaff(message.member, message.guild)) return message.channel.send('You don\'t have permission to do that!');
+		let roleName = args.join(' ');
+		let role = message.guild.roles.find('name', roleName);
+		if (!role) return message.channel.send('Sorry, I can\'t find the role "' + roleName + '"');
 
-function getIsStaff(member) {
-	// TODO: Check for staff roles on member
-}
+		if (!guildData.has(message.guild.id)) storage.buildGuild(message.guild);
+		let guildSettings = guildData.get(message.guild.id);
+		let roleData = (guildSettings.roles) ? guildSettings.roles : {};
+		let allowedRoles = (roleData.whitelist) ? roleData.whitelist : [];
 
-function isColorRole(roleName) {
-	return (/^#(?:[0-9a-fA-F]{3}){1,2}$/.test(roleName));
-}
+		if (allowedRoles.indexOf(roleName) < 0) return message.channel.send('That role isn\'t whitelisted');
+
+		allowedRoles = allowedRoles.slice(0, allowedRoles.indexOf(roleName)).concat(allowedRoles.slice(allowedRoles.indexOf(roleName) + 1));
+		roleData.whitelist = allowedRoles;
+		guildSettings.roles = roleData;
+		guildData.set(message.guild.ind, guildSettings);
+
+		return message.channel.send(roleName + ' was removed from the whitelist :+1:');
+	}
+};
