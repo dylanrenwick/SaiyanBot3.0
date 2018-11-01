77658025fd66d383718575c70f529d7ecebd5791
Dylan Renwick <skidsirongenesisdev@gmail.com>
2018-11-01 10:51:27 -0400

    Restructured database connection to expect type rather than inferring from URI format

--- rss/db/connect.js
+++ rss/db/connect.js
@@ -1,30 +1,21 @@
-const fs = require('fs')
-const config = require('../../config.json')
-const mongoose = require('mongoose')
-const log = require('../../util/logger.js')
-const BUFFER_CONFIGS = ['sslCA', 'sslCRL', 'sslCert', 'sslKey']
-const CON_SETTINGS = typeof config.database.connection === 'object' ? config.database.connection : {}
+const config = require('../../config.json');
+const log = require('../../util/logger.js');
 
 module.exports = callback => {
-  const uri = process.env.DRSS_DATABASE_URI || config.database.uri
-  if (!uri.startsWith('mongo')) return callback() // Means filebase sources will be used
-
-  const buffers = {}
-  if (Object.keys(CON_SETTINGS).length > 0) {
-    for (var x = 0; x < BUFFER_CONFIGS.length; ++x) {
-      const name = BUFFER_CONFIGS[x]
-      if (CON_SETTINGS[name]) buffers[name] = fs.readFileSync(CON_SETTINGS[name])
-    }
-  }
-
-  (function connect () {
-    // Do not callback on .then here since the promise never gets resolved for some reason
-    mongoose.connect(uri, { keepAlive: 120, ...CON_SETTINGS, ...buffers }) // Environment variable in Docker container if available
-    .catch(err => {
-      log.general.error('Failed to connect to database, retrying in 30 seconds...', err)
-      setTimeout(connect, 30000)
-    })
-  })()
-
-  mongoose.connection.once('open', callback)
+	let uri = process.env.DRSS_DATABASE_URI || config.database.uri;
+	let dbType = config.database.type;
+	log.general.info(`Connecting to DB URI ${uri} with type ${dbType}`);
+	switch(dbType) {
+		case 'mongo':
+			// TODO: Add MongoDB Support
+		case 'mysql':
+			// TODO: Add MySQL Support
+		case 'sqlite':
+			// TODO: Add SQLite Support
+		default:
+			log.general.warning(`Unknown or unsupported DB type "${dbType}", defaulting to file storage`);
+		case 'json':
+		case 'file':
+			return callback();
+	}
 }
